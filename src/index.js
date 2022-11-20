1f995dd4f0741a3eff9c75f4a29a4ce7fa443ed1
Plekhov Andrei <fundiexx@gmail.com>
2022-11-20 14:06:06 +0300

    fix: miniDetails

--- src/index.js
+++ src/index.js
@@ -18,9 +18,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'dataSources.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.setFullScreen(true)
 };
 
 // This method will be called when Electron has finished
